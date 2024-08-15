5f381ce26a7ad4c28381e9b42f2ae08ce6cfe784
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-08-15 17:18:22 +0800

    Update Validation of Inquiry Follow Up
    Update Validation of Inquiry Follow Up

--- src/main/java/org/guanzon/auto/validator/sales/Validator_Inquiry_FollowUp.java
+++ src/main/java/org/guanzon/auto/validator/sales/Validator_Inquiry_FollowUp.java
@@ -63,31 +63,70 @@ public class Validator_Inquiry_FollowUp implements ValidatorInterface {
             }
         }
         
-        date = (Date) poEntity.getValue("dFollowUp");
-        if(date == null){
-            psMessage = "Invalid Next Follow Up Date.";
-            return false;
-        } else {
-            if("1900-01-01".equals(xsDateShort(date))){
-                psMessage = "Invalid Next Follow Up Date.";
-                return false;
-            }
-        }
+//        date = (Date) poEntity.getValue("dFollowUp");
+//        if(date == null){
+//            psMessage = "Invalid Next Follow Up Date.";
+//            return false;
+//        } else {
+//            if("1900-01-01".equals(xsDateShort(date))){
+//                psMessage = "Invalid Next Follow Up Date.";
+//                return false;
+//            }
+//        }
+//        
+//        if(poEntity.getFollowUpTme() == null) {
+//            psMessage = "Invalid Next Follow Up Time.";
+//            return false;
+//        } else {
+//            if (poEntity.getFollowUpTme().equals(Time.valueOf("00:00:00")) || String.valueOf(poEntity.getFollowUpTme()).length() < 8){
+//                psMessage = "Invalid Next Follow Up Time.";
+//                return false;
+//            }
+//        }
+//        
+//        int lnHr = Integer.parseInt(String.valueOf(poEntity.getFollowUpTme()).substring(0, 2));
+//        String lnCol1 =String.valueOf(poEntity.getFollowUpTme()).substring(2, 3);
+//        int lnMin = Integer.parseInt(String.valueOf(poEntity.getFollowUpTme()).substring(3, 5));
+//        String lnCol2 =String.valueOf(poEntity.getFollowUpTme()).substring(5, 6);
+//        int lnSs = Integer.parseInt(String.valueOf(poEntity.getFollowUpTme()).substring(6, 8));
+//        
+//        System.out.println("lnHr : " + lnHr);
+//        System.out.println("lnCol1 : " + lnCol1);
+//        System.out.println("lnMin : " + lnMin);
+//        System.out.println("lnCol2 : " + lnCol2);
+//        System.out.println("lnSs : "  + lnSs);
+//        
+//        if(!lnCol1.equals(":") || !lnCol2.equals(":")){
+//                psMessage = "Invalid Next Follow Up Time.";
+//                return false;
+//        }
+//        
+//        if(lnHr < 8 || lnHr > 17){
+//            psMessage = "Invalid Next Follow Up Time working hours.";
+//            return false;
+//        }
+//        
+//        if(lnMin < 0 || lnMin > 59){
+//            psMessage = "Invalid Next Follow Up Time minutes.";
+//            return false;
+//        }
+//        
+//        if(lnSs < 0 || lnSs > 59){
+//            psMessage = "Invalid Next Follow Up Time seconds.";
+//            return false;
+//        }
+//        
+//        if(lnHr == 17){
+//            if(lnMin > 30){
+//                psMessage = "Invalid Next Follow Up Time working hours.";
+//                return false;
+//            }
+//        }
         
-        if(poEntity.getFollowUpTme() == null) {
-            psMessage = "Invalid Next Follow Up Time.";
-            return false;
-        } else {
-            if (poEntity.getFollowUpTme().equals(Time.valueOf("00:00:00"))){
-                psMessage = "Invalid Next Follow Up Time.";
-                return false;
-            }
-        }
-        
-        if(xsDateShort((Date) poEntity.getValue("dFollowUp")).equals(xsDateShort((Date) poEntity.getValue("dTransact")))){
-            psMessage = "Follow Up date cannot be the same with next follow up date.";
-            return false;
-        }
+//        if(xsDateShort((Date) poEntity.getValue("dFollowUp")).equals(xsDateShort((Date) poEntity.getValue("dTransact")))){
+//            psMessage = "Follow Up date cannot be the same with next follow up date.";
+//            return false;
+//        }
         
         if(poEntity.getEmployID()== null) {
             psMessage = "Employee is not set.";
@@ -99,17 +138,25 @@ public class Validator_Inquiry_FollowUp implements ValidatorInterface {
             }
         }
         
-        if(poEntity.getMethodCd().equals("2")){
-            if(poEntity.getSclMedia()== null) {
-                psMessage = "Social Media Platform is not set.";
+        if(poEntity.getRemarks()== null) {
+            psMessage = "Remarks is not set.";
+            return false;
+        } else {
+            if (poEntity.getRemarks().isEmpty()){
+                psMessage = "Remarks is not set.";
                 return false;
-            } else {
-                if (poEntity.getSclMedia().isEmpty()){
-                    psMessage = "Social Media Platform is not set.";
-                    return false;
-                }
             }
         }
+        
+//        if(poEntity.getMessage()== null) {
+//            psMessage = "Message is not set.";
+//            return false;
+//        } else {
+//            if (poEntity.getMessage().isEmpty()){
+//                psMessage = "Message is not set.";
+//                return false;
+//            }
+//        }
         
         if(poEntity.getMethodCd().equals("2")){
             if(poEntity.getSclMedia()== null) {
