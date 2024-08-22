0ea22f4d189ee3cfb0b706c43fdaa903c483fef0
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-08-22 17:19:53 +0800

    Update VSP Master and Added functionality for VSP Finance, Labor and Parts
    Update VSP Master and Added functionality for VSP Finance, Labor and Parts

--- src/main/java/org/guanzon/auto/validator/sales/Validator_VehicleSalesProposal_Finance.java
+++ src/main/java/org/guanzon/auto/validator/sales/Validator_VehicleSalesProposal_Finance.java
@@ -5,30 +5,181 @@
  */
 package org.guanzon.auto.validator.sales;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import org.guanzon.appdriver.base.GRider;
+import org.guanzon.auto.model.sales.Model_VehicleSalesProposal_Finance;
 
 /**
  *
  * @author Arsiela
  */
 public class Validator_VehicleSalesProposal_Finance implements ValidatorInterface {
+    GRider poGRider;
+    String psMessage;
+    
+    Model_VehicleSalesProposal_Finance poEntity;
 
-    public Validator_VehicleSalesProposal_Finance(Object foValue) {
+    Validator_VehicleSalesProposal_Finance(Object foValue){
+        poEntity = (Model_VehicleSalesProposal_Finance) foValue;
     }
 
     @Override
     public void setGRider(GRider foValue) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        poGRider = foValue;
     }
 
     @Override
     public boolean isEntryOkay() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(poEntity.getTransNo()== null) {
+            psMessage = "Transaction No is not set.";
+            return false;
+        } else {
+            if (poEntity.getTransNo().isEmpty()){
+                psMessage = "Transaction No is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getFinPromo()== null) {
+            psMessage = "Finance Promo is not set.";
+            return false;
+        } else {
+            if (poEntity.getFinPromo().trim().isEmpty()){
+                psMessage = "Finance Promo is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getBankID()== null) {
+            psMessage = "Bank ID is not set.";
+            return false;
+        } else {
+            if (poEntity.getBankID().trim().isEmpty()){
+                psMessage = "Bank ID is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getBankname()== null) {
+            psMessage = "Bank Name is not set.";
+            return false;
+        } else {
+            if (poEntity.getBankname().isEmpty()){
+                psMessage = "Bank Name is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getFinAmt() == null) {
+            psMessage = "Finance Amount is not set.";
+            return false;
+        } else {
+            if (poEntity.getFinAmt() <= 0.00){
+                psMessage = "Finance Amount is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getAcctTerm() == null) {
+            psMessage = "Finance Term is not set.";
+            return false;
+        } else {
+            if (poEntity.getAcctTerm() <= 0.00){
+                psMessage = "Finance Term is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getAcctRate() == null) {
+            psMessage = "Finance Rate is not set.";
+            return false;
+        } else {
+            if (poEntity.getAcctRate() <= 0.00){
+                psMessage = "Finance Rate is not set.";
+                return false;
+            }
+        }
+        
+//        if(poEntity.getRebates() == null) {
+//            psMessage = "Finance Rebate is not set.";
+//            return false;
+//        } else {
+//            if (poEntity.getRebates() <= 0.00){
+//                psMessage = "Finance Rebate is not set.";
+//                return false;
+//            }
+//        }
+        
+        if(poEntity.getMonAmort() == null) {
+            psMessage = "Finance Amortization is not set.";
+            return false;
+        } else {
+            if (poEntity.getMonAmort() <= 0.00){
+                psMessage = "Finance Amortization is not set.";
+                return false;
+            }
+        }
+        
+//        if(poEntity.getPNValue() == null) {
+//            psMessage = "Finance Value is not set.";
+//            return false;
+//        } else {
+//            if (poEntity.getPNValue() <= 0.00){
+//                psMessage = "Finance Value is not set.";
+//                return false;
+//            }
+//        }
+//        
+//        if(poEntity.getBnkPaid() == null) {
+//            psMessage = "Finance Bank Pay is not set.";
+//            return false;
+//        } else {
+//            if (poEntity.getBnkPaid() <= 0.00){
+//                psMessage = "Finance Bank Pay is not set.";
+//                return false;
+//            }
+//        }
+        
+        if(poEntity.getGrsMonth() == null) {
+            psMessage = "Finance Gross Amount is not set.";
+            return false;
+        } else {
+            if (poEntity.getGrsMonth() <= 0.00){
+                psMessage = "Finance Gross Amount is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getNtDwnPmt() == null) {
+            psMessage = "Finance Net Downpayment is not set.";
+            return false;
+        } else {
+            if (poEntity.getNtDwnPmt() <= 0.00){
+                psMessage = "Finance Net Downpayment is not set.";
+                return false;
+            }
+        }
+        
+//        if(poEntity.getDiscount() == null) {
+//            psMessage = "Finance Discount is not set.";
+//            return false;
+//        } else {
+//            if (poEntity.getDiscount() <= 0.00){
+//                psMessage = "Finance Discount is not set.";
+//                return false;
+//            }
+//        }
+        
+        return true;
     }
 
+    
     @Override
     public String getMessage() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return psMessage;
     }
     
 }
--- src/main/java/org/guanzon/auto/validator/sales/Validator_VehicleSalesProposal_Labor.java
+++ src/main/java/org/guanzon/auto/validator/sales/Validator_VehicleSalesProposal_Labor.java
@@ -6,29 +6,88 @@
 package org.guanzon.auto.validator.sales;
 
 import org.guanzon.appdriver.base.GRider;
+import org.guanzon.auto.model.sales.Model_VehicleSalesProposal_Labor;
 
 /**
  *
  * @author Arsiela
  */
 public class Validator_VehicleSalesProposal_Labor implements ValidatorInterface {
+    GRider poGRider;
+    String psMessage;
+    
+    Model_VehicleSalesProposal_Labor poEntity;
 
-    public Validator_VehicleSalesProposal_Labor(Object foValue) {
+    Validator_VehicleSalesProposal_Labor(Object foValue){
+        poEntity = (Model_VehicleSalesProposal_Labor) foValue;
     }
 
     @Override
     public void setGRider(GRider foValue) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        poGRider = foValue;
     }
 
     @Override
     public boolean isEntryOkay() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(poEntity.getTransNo()== null) {
+            psMessage = "Transaction No is not set.";
+            return false;
+        } else {
+            if (poEntity.getTransNo().isEmpty()){
+                psMessage = "Transaction No is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getLaborCde()== null) {
+            psMessage = "labor Code is not set.";
+            return false;
+        } else {
+            if (poEntity.getLaborCde().trim().isEmpty()){
+                psMessage = "labor Code is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getLaborDsc()== null) {
+            psMessage = "Labor Description is not set.";
+            return false;
+        } else {
+            if (poEntity.getLaborDsc().trim().isEmpty()){
+                psMessage = "Labor Description is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getChrgeTyp()== null) {
+            psMessage = "Charge Type is not set.";
+            return false;
+        } else {
+            if (poEntity.getChrgeTyp().trim().isEmpty()){
+                psMessage = "Charge Type is not set.";
+                return false;
+            } 
+        }
+        //TODO
+        if(poEntity.getChrgeTyp().equals("1")){
+            if(poEntity.getLaborAmt()== null) {
+                psMessage = "Labor Amount is not set.";
+                return false;
+            } else {
+                if (poEntity.getLaborAmt() <= 0.00){
+                    psMessage = "Labor Amount is not set.";
+                    return false;
+                }
+            }
+        }
+        
+        return true;
     }
 
+    
     @Override
     public String getMessage() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return psMessage;
     }
     
 }
